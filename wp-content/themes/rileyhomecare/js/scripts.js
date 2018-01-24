57528bda2847053cc9d3e7ef2b95ec39c33f9bb4
Matt <matt@mspacecreative.com>
2018-01-24 17:22:06 -0400

    c

--- wp-content/themes/rileyhomecare/js/scripts.js
+++ wp-content/themes/rileyhomecare/js/scripts.js
@@ -1,3 +1,15 @@
 if($.trim($(".et-social-icons").html())=='') {
 	$('.et-social-icons').css('margin', '0');
 }
+
+function pageHeight() {
+	$('#page-container').css('padding-top', $('#main-header').height());
+}
+
+$(document).ready(function () {
+	pageHeight();
+});
+
+$(window).resize(function () {
+	pageHeight();
+});
